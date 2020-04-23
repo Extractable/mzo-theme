d75bdbf88fcb9d60611850d7acd99944bff6d746
Pasquale Scerbo <pscerbo@extractable.com>
2020-04-23 11:50:38 -0700

    Product Selector module img size fix

--- src/js/theme.js
+++ src/js/theme.js
@@ -783,9 +783,9 @@ var eventer = window[eventMethod];
 var messageEvent = eventMethod == "attachEvent" ? "onmessage" : "message";
 // Listen for a message from the iframe.
 eventer(messageEvent, function(e) {
-if (isNaN(e.data)) return;
+    if (isNaN(e.data)) return;
 
-// replace #sizetracker with what ever what ever iframe id you need
-document.getElementById('sizetracker').style.height = e.data + 'px';
+    // replace #sizetracker with what ever what ever iframe id you need
+    document.getElementById('sizetracker').style.height = e.data + 'px';
 
 }, false);
